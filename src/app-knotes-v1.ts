090f93707ea279b8596b9d5b0dcbb9950cd358f3
Klaus Heuschild <klaus.heuschild@atruvia.de>
2022-11-05 12:53:59 +0100

    Dark/Light Switch Cleanup

--- src/app-knotes-v1.ts
+++ src/app-knotes-v1.ts
@@ -1,3 +1,18 @@
 import { AppKnotesV1 } from './AppKnotesV1.js';
 
 customElements.define('app-knotes-v1', AppKnotesV1);
+
+{ // code for switch dark mode
+    const btn = document.querySelector(".btn-toggle");
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (btn) {
+        btn.addEventListener("click", () => {
+            if (prefersDarkScheme.matches) {
+                document.body.classList.toggle("light-theme");
+            } else {
+                document.body.classList.toggle("dark-theme");
+            }
+        });
+    };
+}
